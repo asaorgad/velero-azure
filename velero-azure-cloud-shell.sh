c01f5ea30f1ffb73acb2d052f7458f3f2aa0cd2b
asaorgad <77362455+asaorgad@users.noreply.github.com>
2021-02-11 22:04:18 +0200

    Create velero-azure-cloud-shell.sh

---
+++ velero-azure-cloud-shell.sh
@@ -0,0 +1,7 @@
+
+
+wget https://github.com/vmware-tanzu/velero/releases/download/v1.2.0/velero-v1.2.0-linux-amd64.tar.gz
+tar -zxvf velero-v1.2.0-linux-amd64.tar.gz
+mkdir ~/.velero
+mv velero-v1.2.0-linux-amd64/velero ~/
+export PATH=$PATH:~/.velero/
